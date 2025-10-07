f946b26eb3b234dd2a8a2b0342ea512c3590d3de
Margoohs2 <margaridaunic@gmail.com>
2025-10-06 21:23:09 -0300

    Atributos da entidade Livro.

--- projetoBiblioteca/src/main/java/br/edu/unichristus/projetoBiblioteca/model/Livro.java
+++ projetoBiblioteca/src/main/java/br/edu/unichristus/projetoBiblioteca/model/Livro.java
@@ -2,10 +2,21 @@ package br.edu.unichristus.projetoBiblioteca.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+
+import java.text.spi.DateFormatProvider;
 
 @Entity
 public class Livro {
-    @Id @GeneratedValue
-    // oi tudo bem? este é o nosso projeto.
+    @Id @GeneratedValue (strategy = GenerationType.IDENTITY)
+    // Atributos da entidade Livro
+    private String id;
+    private String titulo;
+    private Double numEdicao;
+    private DateFormatProvider dataPublicacao;
+    private String idAutor;
+    private String idEditora;
+    private Integer paginas;
+    private String idCategoria;
 }
