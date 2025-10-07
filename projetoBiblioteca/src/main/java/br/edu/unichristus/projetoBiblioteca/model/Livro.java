230ed66e5027218c2e867033c4d46d093c54615c
Margoohs2 <margaridaunic@gmail.com>
2025-10-06 21:05:51 -0300

    teste de commit

--- projetoBiblioteca/src/main/java/br/edu/unichristus/projetoBiblioteca/model/Livro.java
+++ projetoBiblioteca/src/main/java/br/edu/unichristus/projetoBiblioteca/model/Livro.java
@@ -1,5 +1,11 @@
 package br.edu.unichristus.projetoBiblioteca.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Livro {
+    @Id @GeneratedValue
     // oi tudo bem? este é o nosso projeto.
 }
